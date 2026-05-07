ce19be08d48eecdba0c392ac6268d532e6335e99
liluwebdev <lilu.webdev@gmail.com>
2026-05-07 11:17:29 -0500

    fixing linking

--- src/pages/curriculum/what-is-art.js
+++ src/pages/curriculum/what-is-art.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Container, Row, Col, Accordion, Button } from "react-bootstrap";
